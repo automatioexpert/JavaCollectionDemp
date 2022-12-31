7d44153424edf7d90a46e7e88e08d3779546215a
Valmiki Vishwakarma <you@example.com>
2022-12-31 11:01:40 +0530

    Pushing all the code changes on 31Dec2022.

---
+++ src/ListDemo/ArrayDemo.java
@@ -0,0 +1,15 @@
+package ListDemo;
+
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+
+        String [] data={"simon","steve","rino","john","Don Breckenridge"};
+        System.out.println(data);
+
+        for(String d:data){
+            System.out.println(d);
+        }
+
+    }
+}
